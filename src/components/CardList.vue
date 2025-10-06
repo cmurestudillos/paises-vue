cdb19b84e0df19451c8f1e293cc2afc81c3836a5
Carlos Mur <cmurdeveloper@gmail.com>
2025-10-06 15:10:42 +0200

    feat: Cambiar el enpoint del que se obtienen los datos

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="row gx-4 gx-lg-5">
-    <div class="col-md-4 mb-5" v-for="pais in paises" :key="pais.name">
-        <Card :pais="pais" />
+    <div class="col-md-4 mb-5" v-for="pais in paises" :key="pais._id || pais.cca3">
+      <Card :pais="pais" />
     </div>
   </div>
 </template>
@@ -9,28 +9,24 @@
 <script>
 import Card from './Card.vue';
 import { computed, onMounted } from 'vue';
-import {useStore} from 'vuex';
+import { useStore } from 'vuex';
 
 export default {
-    name: 'CardList',
-    components: {
-        Card
-    },
-    setup(){
-        const store = useStore()
-        const paises = computed(() => {
-            return store.getters.topPaisesPoblacion
-        })
+  name: 'CardList',
+  components: {
+    Card,
+  },
+  setup() {
+    const store = useStore();
+    const paises = computed(() => {
+      return store.getters.topPaisesPoblacion;
+    });
 
-        onMounted(async() => {
-            await store.dispatch('getPaises')
-            await store.dispatch('filtrarRegion', 'Europe')
-        })
-        return {paises}
-    }
-}
+    onMounted(async () => {
+      await store.dispatch('getPaises');
+      await store.dispatch('filtrarRegion', 'Europe');
+    });
+    return { paises };
+  },
+};
 </script>
-
-<style>
-
-</style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,52 +1,49 @@
-import { createStore } from 'vuex'
+import { createStore } from 'vuex';
 
 export default createStore({
   state: {
     paises: [],
-    paisesFiltrados: []
+    paisesFiltrados: [],
   },
   mutations: {
     setPaises(state, payload) {
-      state.paises = payload
+      state.paises = payload;
     },
     setPaisesFiltrados(state, payload) {
-      state.paisesFiltrados = payload
-    }
+      state.paisesFiltrados = payload;
+    },
   },
   actions: {
     async getPaises({ commit }) {
       try {
-        const res = await fetch('https://restcountries.com/v2/all')
-        const data = await res.json()
-        commit('setPaises', data)
+        const res = await fetch('https://countries-api-service.vercel.app/api/countries');
+        const response = await res.json();
+        commit('setPaises', response.data);
       } catch (error) {
-        console.log(error)
+        console.log(error);
       }
     },
     filtrarRegion({ commit, state }, region) {
-      const filtro = state.paises.filter(pais => 
-        pais.region.includes(region)
-      )
-      commit('setPaisesFiltrados', filtro)
+      const filtro = state.paises.filter(pais => pais.region.includes(region));
+      commit('setPaisesFiltrados', filtro);
     },
     filtroNombre({ commit, state }, texto) {
-      const textoCliente = texto.toLowerCase()
+      const textoCliente = texto.toLowerCase();
       const filtro = state.paises.filter(pais => {
-        const textoApi = pais.name.toLowerCase()
-        if (textoApi.includes(textoCliente)) {
-          return pais
-        }
-      })
-      commit('setPaisesFiltrados', filtro)
-    }
+        // Buscar en el nombre común
+        const textoApi = pais.name.common.toLowerCase();
+        // También buscar en el nombre oficial
+        const textoOficial = pais.name.official.toLowerCase();
+
+        return textoApi.includes(textoCliente) || textoOficial.includes(textoCliente);
+      });
+      commit('setPaisesFiltrados', filtro);
+    },
   },
   getters: {
     topPaisesPoblacion(state) {
-      return state.paisesFiltrados.sort((a, b) =>
-        a.population < b.population ? 1 : -1
-      )
-    }
+      return state.paisesFiltrados.sort((a, b) => (a.population < b.population ? 1 : -1));
+    },
   },
-  modules: {
-  }
-})
+  modules: {},
+});
